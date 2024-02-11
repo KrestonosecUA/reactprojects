46f05ec70f7caefd63f5c08f27ba15d985dacd40
Admin <rushark123@gmail.com>
2024-02-11 18:58:36 +0200

    hw1 lesson2 add image

--- src/components/PostDetails/PostDetails.js
+++ src/components/PostDetails/PostDetails.js
@@ -3,13 +3,18 @@ import style from "./PostDetails.module.css"
 
 const PostDetails = ({postDetails}) => {
 
-    const {id, title, body} = postDetails;
+    const {id, userId, title, body} = postDetails;
 
     return (
         <div className={style.postDetailsBlock}>
-            <h3 className={style.postNumber}>Number: {id}</h3>
+            <h3 className={style.postNumber}>Post number: {id} </h3>
+            <h3 className={style.postNumber}>User number: {userId} </h3>
             <h1 className={style.postTitle}>{title}</h1>
             <div className={style.postText}>{body}</div>
+            <div className={style.imageBlock}>
+                {/* eslint-disable-next-line jsx-a11y/img-redundant-alt */}
+                <img src="https://www.prostir.ua/wp-content/uploads/2020/10/social-1206603_1920.png" alt="photo" className={style.postImage}/>
+            </div>
         </div>
     );
 };
